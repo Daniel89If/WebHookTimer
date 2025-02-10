e853c1dc595fe24e7207ee0989add3de210a4611
Daniel89If <86039859+Daniel89If@users.noreply.github.com>
2025-02-10 20:22:44 +0200

    Add files via upload

---
+++ WebHookTimer.Tests/Controllers/TimerControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using FakeItEasy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebHookTimer.Controllers;
+using WebHookTimer.Interfaces;
+using WebHookTimer.Services;
+
+namespace WebHookTimer.Tests.Controllers
+{
+    public class TimerControllerTests
+    {
+        private readonly TimerRequestManagment _timerManagment;
+        private readonly IDbManager _dbManager;
+        private readonly TimerController _timerController;
+
+        public TimerControllerTests()
+        {
+            _dbManager = A.Fake<IDbManager>();
+            _timerManagment = new TimerRequestManagment(_dbManager);
+            _timerController = new TimerController(_timerManagment);
+        }
+
+
+        [Fact]
+        public void GetTimerById_SendInt_ReturnNotFound()
+        {
+            var id = "";
+            NotFoundResult result = (NotFoundResult)_timerController.GetTimerById(id);
+            
+            Assert.Equal(StatusCodes.Status404NotFound, result.StatusCode);
+        }
+    }
+}
---
+++ WebHookTimer.Tests/Services/ExpiredTimeHandlerTests.cs
@@ -0,0 +1,29 @@
+﻿using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using WebHookTimer.Interfaces;
+using WebHookTimer.Models;
+using WebHookTimer.Services;
+
+namespace WebHookTimer.Tests.Services
+{
+    public class ExpiredTimeHandlerTests
+    {
+        private readonly IExpiredTime _expiredTime;
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly ILogger<ExpiredTimeHandler> _logger;
+
+        public ExpiredTimeHandlerTests()
+        {
+            _clientFactory = A.Fake<IHttpClientFactory>();
+            _logger = A.Fake<ILogger<ExpiredTimeHandler>>();
+            _expiredTime = new ExpiredTimeHandler(_clientFactory, _logger);
+        }
+
+        [Fact]
+        public void Handle_EmptyList_DoNotActivSendTimerEndResponse()
+        {
+            List<TimerInfo> timerInfos = new List<TimerInfo>();
+            _expiredTime.Handle(timerInfos);
+        }
+    }
+}
